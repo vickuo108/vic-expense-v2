3416e104ad794cd440a7b9ac4e09115f520a55ee
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-08-05 23:28:45 +0800

    Refine record filter select mode

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vic-v2-67';
+const CACHE = 'vic-v2-68';
 const APP_SHELL = ['./', './index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL)));
