0094bff96243584994b2987ef5d56424236e5d9a
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-08-14 13:51:36 +0800

    Refine credit card detail layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vic-v2-69';
+const CACHE = 'vic-v2-70';
 const APP_SHELL = ['./', './index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL)));
