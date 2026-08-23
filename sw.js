3844270f24cc6bbbf58aa727084d2015d8d888b8
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-08-24 01:34:05 +0800

    Improve girlfriend repayment reporting

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vic-v2-73';
+const CACHE = 'vic-v2-74';
 const APP_SHELL = ['./', './index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL)));
