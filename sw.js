36b51327b26e610a9e2af42d17db1c9baf1d7f47
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-08-27 17:09:10 +0800

    Restore category-specific main options

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vic-v2-75';
+const CACHE = 'vic-v2-76';
 const APP_SHELL = ['./', './index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL)));
