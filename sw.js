3ee298ce2b6e7834857dda28a077b921e37c7a9e
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-08-27 17:18:14 +0800

    Hide transfer fee for internal transfers

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'vic-v2-76';
+const CACHE = 'vic-v2-77';
 const APP_SHELL = ['./', './index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL)));
